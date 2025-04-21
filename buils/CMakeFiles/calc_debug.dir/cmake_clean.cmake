4b55a353df0107a8ce68e8c514047ef9dac63e01
dotharer <dotharer@vs.dotharer>
2025-04-21 06:36:06 +0300

    calc

---
+++ buils/CMakeFiles/calc_debug.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc_debug.dir/calc.c.o"
+  "CMakeFiles/calc_debug.dir/calc.c.o.d"
+  "CMakeFiles/calc_debug.dir/main.c.o"
+  "CMakeFiles/calc_debug.dir/main.c.o.d"
+  "calc_debug"
+  "calc_debug.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calc_debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "define.h"
-
-int main() {
-  
-  ERROR("dddd",1);
-}
